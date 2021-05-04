c28de66b210d2a51ee16bb4c134451f35d76294b
Mako <hal.m.90215@gmail.com>
2021-05-05 02:45:43 +0900

    アイコンの変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#477294`,
         display: `standalone`,
-        icon: `src/images/mako-icon.jpg`,
+        icon: `src/images/NanaIro-Week-icon.png`,
       },
     },
   ],
