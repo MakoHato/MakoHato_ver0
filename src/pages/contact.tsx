8d89792a9315d737d7f982c8e2a8c6311ec8870b
Mako <hal.m.90215@gmail.com>
2021-05-06 23:49:19 +0900

    Update contact.tsx

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -3,13 +3,21 @@ import NanaLayout from "../components/layout"
 
 import SEO from "../components/seo";
 
+import "../styles/contact.scss"
+
 export default function Contact({location}) {
   return (
     <NanaLayout>
-      <SEO pagetitle="ページが見つかりません" pagepath={location.pathname}/>
-      <h1 style={{ padding: "20vh 0", textAlign: "center" }}>
-        お探しのページは見つかりませんでした(´・ω・｀ )
-      </h1>
+      <SEO pagetitle="CONTACT" pagepath={location.pathname}/>
+      <div className="Contact-Page h-100vh">
+        <h2 className="txt-al-c pd-tb-50 fs-2r">CONTACT</h2>
+        <div className="w-700 mg-c">
+          <span className="black-bar-1"></span>
+        </div>
+        <p className="">
+          メールアドレス:nanairo.week@gmail.com
+        </p>
+      </div>
     </NanaLayout>
   )
 }
