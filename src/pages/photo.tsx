f21dc6f6b18f64434510e1bb4cf868d6fc9bbc99
Mako <hal.m.90215@gmail.com>
2021-05-06 21:11:01 +0900

    各ページの更新

--- src/pages/photo.tsx
+++ src/pages/photo.tsx
@@ -3,13 +3,24 @@ import NanaLayout from "../components/layout"
 
 import SEO from "../components/seo";
 
+import "../styles/photo.scss"
+
 export default function Photo({location}) {
   return (
     <NanaLayout>
-      <SEO pagetitle="VIDEO" pagepath={location.pathname}/>
-      <h3 style={{ padding: "20vh 0", textAlign: "center" }}>
-        コンテンツがまだありません。
-      </h3>
+      <SEO pagetitle="PHOTO" pagepath={location.pathname}/>
+      <div className="Photo-Area">
+        <h2 className="txt_al-c pd_tb-50 fs_2">PHOTO</h2>
+        <div className="w-700 mg_c">
+          <span className="b_bar-700-1"></span>
+        </div>
+        <div>
+          <p className="txt_al-c fs_1-2 pd_tb-100">
+            コンテンツがまだありません。<br />
+            今後の更新をお待ちくださいませ。
+          </p>
+        </div>
+      </div>
     </NanaLayout>
   )
 }
--- src/pages/video.tsx
+++ src/pages/video.tsx
@@ -3,15 +3,23 @@ import NanaLayout from "../components/layout"
 
 import SEO from "../components/seo";
 
+import "../styles/video.scss"
+
 export default function Video({location}) {
   return (
     <NanaLayout>
       <SEO pagetitle="VIDEO" pagepath={location.pathname}/>
       <div className="Video-Area">
-        <h2>VIDEO</h2>
-        <h3 style={{ padding: "20vh 0", textAlign: "center" }}>
-          コンテンツがまだありません。
-        </h3>
+        <h2 className="txt_al-c pd_tb-50 fs_2">VIDEO</h2>
+        <div className="w-700 mg_c">
+          <span className="b_bar-700-1"></span>
+        </div>
+        <div >
+          <p className="txt_al-c fs_1-2 pd_tb-100">
+            コンテンツがまだありません。<br />
+            今後の更新をお待ちくださいませ。
+          </p>
+        </div>
       </div>
     </NanaLayout>
   )
