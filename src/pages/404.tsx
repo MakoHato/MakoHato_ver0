b8f6ade47a5193403047bcda5b6288f8ec71096e
Mako <hal.m.90215@gmail.com>
2021-05-06 23:47:38 +0900

    404ページ更新

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -7,9 +7,17 @@ export default function NotFound({location}) {
   return (
     <NanaLayout>
       <SEO pagetitle="ページが見つかりません" pagepath={location.pathname}/>
-      <h1 style={{ padding: "20vh 0", textAlign: "center" }}>
-        お探しのページは見つかりませんでした(´・ω・｀ )
-      </h1>
+      <div className="404-Page h-100vh">
+        <h2 className="txt-al-c pd-tb-50 fs-2r">404:NotFound</h2>
+        <div className="w-700 mg-c">
+          <span className="black-bar-1"></span>
+        </div>
+        <div>
+          <p className="txt-al-c fs-1-2r pd-tb-100">
+            お探しのページは見つかりませんでした(´・ω・｀ )
+          </p>
+        </div>
+      </div>
     </NanaLayout>
   )
 }
