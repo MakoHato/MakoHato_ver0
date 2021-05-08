255c185ca36b6435305f32a8f54791025a83692c
Mako <hal.m.90215@gmail.com>
2021-05-09 04:29:47 +0900

    indexをlayoutを使わずに表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import React from "react"
-import NanaLayout from "../components/layout"
 import { TwitterTimelineEmbed } from "react-twitter-embed"
 
 import { useWindowDimensions } from "../Hooks/Window-Size"
 
+import NanaLayout from "../components/layout"
+import HumbergerMenu from "../components/Humberger-Menu"
+import NanaHeader from "../components/header"
+import NanaFooter from "../components/footer"
+
 import SEO from "../components/seo";
+import MainVisual from "../components/MainVisual"
 
 import "../styles/Mako-Flame.scss"
 import "../styles/pages/index.scss"
@@ -12,70 +17,83 @@ import "../styles/pages/index.scss"
 export default function Home({location}) {
   const { width } = useWindowDimensions();
   return (
-    <NanaLayout>
+    <div>
+
+      <div className="disp">
+        <HumbergerMenu />
+      </div>
+
+      <NanaHeader />
+
       <SEO />
-      <div className="Blog-Area">
-        <h2>Blog</h2>
-        <div className="w-70p mg-c">
-          <span className="black-bar-1"></span>
-        </div>
-        <div className="txt-al-c">
-          <p className="pd-tb-100">
-            コンテンツがまだありません。<br />
-            今後の更新をお待ちください。
-          </p>
-        </div>
-      </div>
-      <div className="Photo-Area">
-        <h2>Photo</h2>
-        <div className="w-70p mg-c">
-          <span className="black-bar-1"></span>
-        </div>
-        <div className="txt-al-c">
-          <p className="pd-tb-100">
-            コンテンツがまだありません。<br />
-            今後の更新をお待ちください。
-          </p>
-        </div>
-      </div>
-      <div className="Twitter-Area">
-        <h2>Twitter</h2>
-        <div className="w-70p mg-c">
-          <span className="black-bar-1"></span>
-        </div>
-        <div className="Timeline-Area">
-          <div className="Nana-T">
-            <h3>NanaIroWeek's ACCOUNT</h3>
-            { width > 769 ?
-              <TwitterTimelineEmbed
-                sourceType="profile"
-                screenName="nanairo_week"
-                options={{ width: 400, height: 700}}
-              />
-              :<TwitterTimelineEmbed
-                sourceType="profile"
-                screenName="nanairo_week"
-                options={{ width: 300, height: 400}}
-              />
-            }
+
+      <MainVisual/>
+
+      <div className="page">
+        <div className="Blog-Area">
+          <h2>Blog</h2>
+          <div className="w-70p mg-c">
+            <span className="black-bar-1"></span>
           </div>
-          <div className="Mako-T">
-            <h3>Mako's ACCOUNT</h3>
-            { width > 769 ?
-              <TwitterTimelineEmbed
-                sourceType="profile"
-                screenName="Mak_o_"
-                options={{ width: 400, height: 700}}
-              />
-              :<TwitterTimelineEmbed
-                sourceType="profile"
-                screenName="Mak_o_"
-                options={{ width: 300, height: 400}}
-              />
-            }
+          <div className="txt-al-c">
+            <p className="pd-tb-100">
+              コンテンツがまだありません。<br />
+              今後の更新をお待ちください。
+            </p>
           </div>
         </div>
+        <div className="Photo-Area">
+          <h2>Photo</h2>
+          <div className="w-70p mg-c">
+            <span className="black-bar-1"></span>
+          </div>
+          <div className="txt-al-c">
+            <p className="pd-tb-100">
+              コンテンツがまだありません。<br />
+              今後の更新をお待ちください。
+            </p>
+          </div>
+        </div>
+        <div className="Twitter-Area">
+          <h2>Twitter</h2>
+          <div className="w-70p mg-c">
+            <span className="black-bar-1"></span>
+          </div>
+          <div className="Timeline-Area">
+            <div className="Nana-T">
+              <h3>NanaIroWeek's ACCOUNT</h3>
+              { width > 769 ?
+              <TwitterTimelineEmbed
+              sourceType="profile"
+              screenName="nanairo_week"
+              options={{ width: 400, height: 700}}
+              />
+              :<TwitterTimelineEmbed
+              sourceType="profile"
+              screenName="nanairo_week"
+              options={{ width: 300, height: 400}}
+              />
+              }
+            </div>
+            <div className="Mako-T">
+              <h3>Mako's ACCOUNT</h3>
+              { width > 769 ?
+              <TwitterTimelineEmbed
+              sourceType="profile"
+              screenName="Mak_o_"
+              options={{ width: 400, height: 700}}
+              />
+              :<TwitterTimelineEmbed
+              sourceType="profile"
+              screenName="Mak_o_"
+              options={{ width: 300, height: 400}}
+              />
+              }
+            </div>
+          </div>
+        </div>
       </div>
-    </NanaLayout>
+      <NanaFooter />
+    </div>
   )
 }
