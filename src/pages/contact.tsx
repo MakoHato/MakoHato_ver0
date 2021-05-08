e792ad4f6408328461e29eda9cb546205e93a185
Mako <hal.m.90215@gmail.com>
2021-05-09 03:12:35 +0900

    CONTACTページのスタイルを適用。

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -11,7 +11,7 @@ export default function Contact({location}) {
     <NanaLayout>
       <SEO pagetitle="CONTACT" pagepath={location.pathname}/>
       <div className="Contact-Page h-100vh">
-        <h2 className="txt-al-c pd-tb-50 fs-2r">CONTACT</h2>
+        <h2 className="contact-h2">CONTACT</h2>
         <div className="w-70p mg-c">
           <span className="black-bar-1"></span>
         </div>
@@ -40,19 +40,19 @@ export default function Contact({location}) {
               </p>
               <input type="email" className="form-control" id="email" name="email" placeholder="" pattern="^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$" required autoComplete="email" />
               </label>
-              </div>
+            </div>
             <div className="pd-tb-20">
               <label>
               <p className="pd-b-10">
                 お問い合わせ内容
               </p>
-              <textarea className="form-control" id="contact" name="content" rows={8} required></textarea>
+              <textarea className="form-control" id="contact" name="content" rows={20} required></textarea>
               </label>
             </div>
           </div>
 
           <div className="txt-al-c">
-          <button type="submit">送信</button>
+            <button type="submit" className="contact-bt">送信</button>
           </div>
         </form>
       </div>
