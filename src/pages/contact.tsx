8abd0b8c086239027e1c912220974e187c9840c2
Mako <hal.m.90215@gmail.com>
2021-05-08 22:57:25 +0900

    Update contact.tsx

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -16,42 +16,45 @@ export default function Contact({location}) {
           <span className="black-bar-1"></span>
         </div>
         <form
-        name="contact"
-        method="POST"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
         >
           <input type="hidden" name="form-name" value="contact" />
           <input type="hidden" name="bot-field" />
 
           <div className="contact-form txt-al-c">
-            <label>
-            <p>
-              お名前
-            </p>
+            <div className="pd-tb-20">
+              <label>
+              <p className="pd-b-10">
+                お名前
+              </p>
               <input type="text" className="form-control" id="name" name="name" placeholder="お名前" maxLength={30} minLength={2} required autoComplete="name" />
-            </label>
-            <label>
-              <p>
+              </label>
+            </div>
+            <div className="pd-tb-20">
+              <label>
+              <p className="pd-b-10">
                 メールアドレス
               </p>
-            <input type="email" className="form-control" id="email" name="email" placeholder="" pattern="^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$" required autoComplete="email" />
-          </label>
-            <label>
-            <p>
-              お問い合わせ内容
-            </p>
-            <textarea className="form-control" id="contact" name="content" rows={8} required></textarea>
-            </label>
+              <input type="email" className="form-control" id="email" name="email" placeholder="" pattern="^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$" required autoComplete="email" />
+              </label>
+              </div>
+            <div className="pd-tb-20">
+              <label>
+              <p className="pd-b-10">
+                お問い合わせ内容
+              </p>
+              <textarea className="form-control" id="contact" name="content" rows={8} required></textarea>
+              </label>
+            </div>
           </div>
 
           <div className="txt-al-c">
           <button type="submit">送信</button>
           </div>
         </form>
-        <div className="txt-al-c">
-          <Link to="/">home</Link>
-        </div>
       </div>
     </NanaLayout>
   )
