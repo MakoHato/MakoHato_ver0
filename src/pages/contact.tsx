780ea7eb0a5936685d37104f065734e1d2ace675
Mako <hal.m.90215@gmail.com>
2021-05-08 19:11:20 +0900

    contactPage 追加

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from 'gatsby'
 import NanaLayout from "../components/layout"
 
 import SEO from "../components/seo";
@@ -14,9 +15,38 @@ export default function Contact({location}) {
         <div className="w-70p mg-c">
           <span className="black-bar-1"></span>
         </div>
-        <p className="txt-al-c pd-tb-100 fs-1-2r">
-          メールアドレス:nanairo.week@gmail.com
-        </p>
+        <h1>Contact Form</h1>
+        <p>メールフォーム送信テスト</p>
+        <form
+        name="contact"
+        method="POST"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        >
+          <input type="hidden" name="form-name" value="contact" />
+          <input type="hidden" name="bot-field" />
+
+          <div className="form-group">
+            <label>お名前<abbr title="required">*</abbr>
+              <input type="text" className="form-control" id="name" name="name" placeholder="お名前" maxLength={30} minLength={2} required autoComplete="name" />
+            </label>
+          </div>
+          <div className="form-group">
+            <label>メールアドレス<abbr title="required">*</abbr>
+            <input type="email" className="form-control" id="email" name="email" placeholder="" pattern="^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$" required autoComplete="email" />
+          </label>
+          </div>
+          <div className="form-group">
+            <label>お問い合わせ内容<abbr title="required">*</abbr>
+            <textarea className="form-control" id="contact" name="content" rows={8} required></textarea>
+            </label>
+          </div>
+
+          <div className="form-group">
+          <button type="submit">送信</button>
+          </div>
+        </form>
+        <Link to="/">home</Link>
       </div>
     </NanaLayout>
   )
