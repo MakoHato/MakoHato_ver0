d68c84b205e5e8376dfc6481cf798085665a9f0d
Mako <hal.m.90215@gmail.com>
2021-05-10 21:13:10 +0900

    ブログ記事ページの生成、タイトルの表示

--- src/pages/blog_post.tsx
+++ src/pages/blog_post.tsx
@@ -1,12 +1,24 @@
 import * as React from "react"
+import { graphql } from "gatsby"
 
 import NanaLayout from "../components/layout"
 import SEO from "../components/seo";
 
 
-export default function BLOG_POST() {
-  <NanaLayout>
-    <SEO />
+export default function BLOG_POST({ data }) {
+  return (
+    <NanaLayout>
+      <SEO />
 
-  </NanaLayout>
+      <h2>{data.contentfulArticles.title}</h2>
+    </NanaLayout>
+  )
 }
+
+export const query:void = graphql`
+  query {
+    contentfulArticles {
+      title
+    }
+  }
+`
