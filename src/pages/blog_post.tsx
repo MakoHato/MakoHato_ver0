1b39668eb2ca068da2009fe20fee1125cf082126
Mako <hal.m.90215@gmail.com>
2021-05-10 19:04:09 +0900

    ブログポストページの作成

---
+++ src/pages/blog_post.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+import NanaLayout from "../components/layout"
+import SEO from "../components/seo";
+
+
+export default function BLOG_POST() {
+  <NanaLayout>
+    <SEO />
+
+  </NanaLayout>
+}
