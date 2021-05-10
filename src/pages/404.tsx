39d4a65e559c20b75ebb7510e457b93139087b6b
Mako <hal.m.90215@gmail.com>
2021-05-10 18:59:17 +0900

    スタイル変更

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -9,9 +9,7 @@ export default function NotFound({location}) {
       <SEO pagetitle="ページが見つかりません" pagepath={location.pathname}/>
       <div className="404-Page h-100vh">
         <h2 className="txt-al-c pd-tb-50 fs-2r">404:NotFound</h2>
-        <div className="w-70p mg-c">
-          <span className="black-bar-1"></span>
-        </div>
+        <span className="bar-666-1"></span>
         <div>
           <p className="txt-al-c fs-1-2r pd-tb-100">
             お探しのページは見つかりませんでした(´・ω・｀ )
