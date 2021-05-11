95e77d0ba1e630c60129d50a270a14c3f73d52ee
Mako <hal.m.90215@gmail.com>
2021-05-11 21:52:12 +0900

    クエリ文の追加

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,4 +2,22 @@ const path = require("path")
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPages } = actions
+
+  const articleresult = await graphql(`
+    query {
+      allContentfulArticles(sort: { fields: createdArticleDate, order: DESC }) {
+        edges {
+          node {
+            id
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  if (articleresult.errors) {
+    reporter.panicOnBuild(`GraphQLのクエリでエラーが発生しました。`)
+    return
+  }
 }
