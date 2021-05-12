03c891614d0911ead8e536f66c5da97a6174862c
Mako <hal.m.90215@gmail.com>
2021-05-12 22:52:23 +0900

    各記事のタイトル表示

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,11 +1,12 @@
 import React from "react"
+import { graphql } from "gatsby"
 import NanaLayout from "../components/layout"
 
 import SEO from "../components/seo";
 
 import "../styles/pages/blog.scss"
 
-export default function Blog({location}) {
+export default function Blog({ data, location }) {
   return (
     <NanaLayout>
       <SEO pagetitle="BLOG" pagepath={location.pathname}/>
@@ -13,12 +14,28 @@ export default function Blog({location}) {
         <h2 className="txt-al-c pd-tb-50 fs-2r">BLOG</h2>
         <span className="bar-666-1"></span>
         <div>
-          <p className="txt-al-c fs-1-2r pd-tb-100">
-            コンテンツがまだありません。<br />
-            今後の更新をお待ちくださいませ。
-          </p>
+          {data.allContentfulArticles.edges.map(({ node }) => (
+            <article>
+              <a>
+                <img></img>
+                <h3>{ node.title }</h3>
+              </a>
+            </article>
+          ))}
         </div>
       </div>
     </NanaLayout>
   )
 }
+
+export const query = graphql`
+  query {
+    allContentfulArticles(sort: { order: DESC, fields: createdArticleDate }) {
+      edges {
+        node {
+          title
+        }
+      }
+    }
+  }
+`
